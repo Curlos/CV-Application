d0a2e1f6241481526b7e827ebf5b209aab5bacae
Carlos Martinez <curlosmart@gmail.com>
2021-08-27 23:39:02 -0400

    experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,13 @@
-const Experience = () => {
+const Experience = ({ experience, handleChangeExperience }) => {
+  
   return (
-    <div>
-      
+    <div className="formInfo experience">
+      <div>Experience</div>
+      <input type="text" name="position" onChange={handleChangeExperience} placeholder="Position" value={experience['position']}/>
+      <input type="text" name="company" onChange={handleChangeExperience} placeholder="Company" value={experience['company']}/>
+      <input type="text" name="city" onChange={handleChangeExperience} placeholder="City" value={experience['city']}/>
+      <input type="text" name="from" onChange={handleChangeExperience} placeholder="From" value={experience['from']}/>
+      <input type="text" name="to" onChange={handleChangeExperience} placeholder="To" value={experience['to']}/>
     </div>
   )
 }
