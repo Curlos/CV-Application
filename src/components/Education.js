ec4a4ed01a35bcc9d1ece16730cb272f81d9cb5f
Carlos Martinez <curlosmart@gmail.com>
2021-08-27 23:38:55 -0400

    education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,13 @@
-const Education = () => {
+const Education = ({ education, handleChangeEducation }) => {
+  
   return (
-    <div>
-      
+    <div className="formInfo experience">
+      <div>Education</div>
+      <input type="text" name="universityName" onChange={handleChangeEducation} placeholder="University name" value={education['universityName']}/>
+      <input type="text" name="city" onChange={handleChangeEducation} placeholder="Company" value={education['city']}/>
+      <input type="text" name="degree" onChange={handleChangeEducation} placeholder="City" value={education['degree']}/>
+      <input type="text" name="from" onChange={handleChangeEducation} placeholder="From" value={education['from']}/>
+      <input type="text" name="to" onChange={handleChangeEducation} placeholder="To" value={education['to']}/>
     </div>
   )
 }
