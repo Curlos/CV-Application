70e596bf660284547d4c04405d94877cc2919ee0
Carlos Martinez <curlosmart@gmail.com>
2021-08-27 20:43:39 -0400

    components setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+import Preview from './components/Preview'
 import './style.css';
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    firstName: '',
+    lastName: '',
+    title: '',
+    photo: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    description: '',
+  })
+  const [experience, setExperience] = useState({})
+  const [education, setEducation] = useState({})
+
+  const handleChangePersonalInfo = (e) => {
+    e.preventDefault()
+    const id = e.target.getAttribute('id')
+    personalInfo
+    setPersonalInfo({ })
+
+  }
+
   return (
-    <div>
-      hello
+    <div className="mainContainer">
+      <Header />
+      <Form personalInfo={personalInfo} experience={experience} education={education} handleChangePersonalInfo={handleChangePersonalInfo}/>
+      <Preview />
     </div>
   );
 }
