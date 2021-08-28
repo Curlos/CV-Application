a70f2afaafd63627c7e60c873ea290dd585b45c3
Carlos Martinez <curlosmart@gmail.com>
2021-08-27 23:38:41 -0400

    PersonalInfo component basic structure fleshed out

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,9 +1,16 @@
 const PersonalInfo = ({ personalInfo, handleChangePersonalInfo }) => {
-  console.log(personalInfo)
   
   return (
-    <div>
-      <input type="text" id="firstName" onChange={handleChangePersonalInfo}></input>
+    <div className="formInfo personalInfo">
+      <div>Personal Information</div>
+      <input type="text" name="firstName" onChange={handleChangePersonalInfo} placeholder="First name" value={personalInfo['firstName']}/>
+      <input type="text" name="lastName" onChange={handleChangePersonalInfo} placeholder="Last name" value={personalInfo['lastName']}/>
+      <input type="text" name="title" onChange={handleChangePersonalInfo} placeholder="Title" value={personalInfo['title']}/>
+      <input type="text" name="photo" onChange={handleChangePersonalInfo} placeholder="Photo" value={personalInfo['photo']}/>
+      <input type="text" name="address" onChange={handleChangePersonalInfo} placeholder="Address" value={personalInfo['address']}/>
+      <input type="text" name="phoneNumber" onChange={handleChangePersonalInfo} placeholder="Phone number" value={personalInfo['phoneNumber']}/>
+      <input type="text" name="email" onChange={handleChangePersonalInfo} placeholder="Email" value={personalInfo['email']}/>
+      <input type="text" name="description" onChange={handleChangePersonalInfo} placeholder="Description" value={personalInfo['description']}/>
     </div>
   )
 }
